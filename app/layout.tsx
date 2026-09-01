56ed5ff9c606aca9c465243d6ddb6f22fefb8e81
yy933 <116449560+yy933@users.noreply.github.com>
2026-09-01 13:32:18 +0800

    feat: update favicon and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,22 @@
 import { Carter_One, Roboto_Slab } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "PopChoice",
+  description: "Find the perfect movie for your group",
+
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: [
+      { url: "/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+  },
+  manifest: "/favicon/site.webmanifest",
+};
 
 const carterOne = Carter_One({
   weight: "400",
